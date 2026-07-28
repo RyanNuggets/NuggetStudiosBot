66a5a98d903a60978ca0f1660fab628a567cd90a
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2026-07-29 01:57:39 +0400

    Update welcome.js

--- Features/welcome.js
+++ Features/welcome.js
@@ -1,57 +1,47 @@
-const { ButtonBuilder, ButtonStyle, ActionRowBuilder } = require("discord.js");
-const { get } = require("http");
+import { ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js";
 
-module.exports = {
-    name: "guildMemberAdd",
-    once: false,
-    async execute(client, member) {
-        const welcomeChannelId = "1485805832519159858";
-        const welcomeChannel = member.guild.channels.cache.get(welcomeChannelId);
+const welcomeChannelId = "1485805832519159858";
 
-        const guild = member.guild;
-        const membercount = guild.memberCount;
+async function handleGuildMemberAdd(member) {
+    const guild = member.guild;
+    const welcomeChannel = guild.channels.cache.get(welcomeChannelId);
+    const membercount = guild.memberCount;
 
-        // const roleId1 = "1498946139070660609";
-// const role1 = guild.roles.cache.get(roleId1);
-
-// if (role1) {
-//     try {
-//         await member.roles.add(role1);
-//         console.log(`Assigned role ${role1.name} to ${member.user.tag}`);
-//     } catch (error) {
-//         console.error(`Failed to assign role1 to ${member.user.tag}: ${error}`);
-//     }
-// } else {
-//     console.error(`Role with ID ${roleId1} not found.`);
-// }
-
-        if (welcomeChannel) {
-            const welcomeMessage = `Welcome to **:nsgreen: Nugget Studios**, ${member}! We are now at **${membercount}** members.`;
-
-            const mcButton = new ButtonBuilder()
-                .setCustomId('mcButton')
-                .setStyle(ButtonStyle.Secondary)
-                .setEmoji({
-                id: "1527102715371716688",
-                name: "nsgreen"
-                })
-                .setLabel(membercount.toString())
-                .setDisabled(true);
-
-                const getStartedButton = new ButtonBuilder()
-                .setStyle(ButtonStyle.Link)
-                .setLabel('Dashboard')
-              .setURL('https://discord.com/channels/1015000518889853028/1486640714128298135');            
-
-            const row = new ActionRowBuilder().addComponents(mcButton, getStartedButton);
-
-            try {
-                await welcomeChannel.send({ content: `${welcomeMessage}`, components: [row] });
-            } catch (error) {
-                console.error(`Failed to send welcome message: ${error}`);
-            }
-        } else {
-            console.error(`Channel with ID ${welcomeChannelId} not found.`);
-        }
+    if (!welcomeChannel) {
+        console.error(`Channel with ID ${welcomeChannelId} not found.`);
+        return;
     }
-};
+
+    const welcomeMessage = `Welcome to **:nsgreen: Nugget Studios**, ${member}! We are now at **${membercount}** members.`;
+
+    const mcButton = new ButtonBuilder()
+        .setCustomId("mcButton")
+        .setStyle(ButtonStyle.Secondary)
+        .setEmoji({
+            id: "1527102715371716688",
+            name: "nsgreen"
+        })
+        .setLabel(membercount.toString())
+        .setDisabled(true);
+
+    const getStartedButton = new ButtonBuilder()
+        .setStyle(ButtonStyle.Link)
+        .setLabel("Dashboard")
+        .setURL("https://discord.com/channels/1015000518889853028/1486640714128298135");
+
+    const row = new ActionRowBuilder().addComponents(mcButton, getStartedButton);
+
+    try {
+        await welcomeChannel.send({ content: welcomeMessage, components: [row] });
+    } catch (error) {
+        console.error(`Failed to send welcome message: ${error}`);
+    }
+}
+
+export default function registerWelcomeModule(client) {
+    client.on("guildMemberAdd", (member) => {
+        handleGuildMemberAdd(member).catch((err) =>
+            console.error("Unhandled error in welcome module:", err)
+        );
+    });
+}
